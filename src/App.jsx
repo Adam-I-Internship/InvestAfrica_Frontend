63d868ed4beff929648bc781697d2d29940f14bd
Sanjay <146640686+san-jay-14@users.noreply.github.com>
2024-05-25 00:16:17 +0530

    setting up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import HomePage from './HomePage';
+// import AboutPage from './AboutPage';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div>
+      <h1>This is my static website</h1>
+      {/* <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+        </Switch>
+      </Router> */}
+    </div>
+  );
+}
 
 export default App;
